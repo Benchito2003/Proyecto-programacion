c928283dd69984ab023cb8ae60a38d3a68a92bfb
RubenAG <rubenalfonsogamboa@gmail.com>
2024-10-23 17:44:35 -0600

    scrollbar y gif de ejemplo

---
+++ imagenes.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+from PIL import Image
+
+root = tk.Tk()
+root.title("Displaing Gif")
+
+file = "Rain.gif"
+info = Image.open(file)
+
+frames = info.n_frames # number of frames
+
+photoimage_objects = []
+for i in range(frames):
+    obj = tk.PhotoImage(file=file, format=f"gif -index {i}")
+    photoimage_objects.append(obj)
+
+
+def animation(current_frame=0):
+    global loop
+    image = photoimage_objects[current_frame]
+    
+    gif_label.configure(image=image)
+    current_frame = current_frame +1
+    
+    if current_frame == frames:
+        current_frame=0
+    
+    loop = root.after(50, lambda: animation(current_frame))
+
+
+def stop_animation():
+    root.after_cancel(loop)
+
+
+gif_label = tk.Label(root, image="")
+gif_label.pack()
+
+start = tk.Button(root, text="Start", command=lambda: animation(current_frame=0))
+start.pack()
+
+stop = tk.Button(root, text="Stop", command=stop_animation)
+stop.pack()
+
+root.mainloop
---
+++ scrollbar.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+root = Tk()
+scrollbar = Scrollbar(root)
+scrollbar.pack(side=RIGHT, fill=Y)
+
+mylist = Listbox(root, yscrollcommand=scrollbar.set)
+for line in range(100):
+    mylist.insert(END, "This is line number "+ str(line))
+
+mylist.pack(side=LEFT, fill= BOTH)
+scrollbar.config(command=mylist.yview)
+
+mainloop()
+
+#NOTA: instalar openCV
+#import cv2
