22d38d061621fa89e06842206b1b920c6bc73589
RubenAG <rubenalfonsogamboa@gmail.com>
2024-10-25 18:03:59 -0600

    Con open cv

--- libreria_opencv.py
+++ libreria_opencv.py
@@ -9,16 +9,19 @@ while(1):
     #convert BGR tho HSV
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     
-    # define range of bliue color in HSV
+    # define range of blue color in HSV
     lower_blue = np.array([110,50,50])
     upper_blue = np.array([130,255,255])
     
-    gris_claro = np.array([ 216, 216, 216 ])
     gris_oscuro = np.array([ 55, 55, 55])
+    gris_claro = np.array([ 216, 216, 216 ])
+    
+    verde_oscuro = np.array([110,50,50])
+    verde_claro = np.array([0, 0, 255])
     
     # Threshold the HSV image to get only blue colors
     #mask = cv.inRange(hsv,lower_blue, upper_blue)
-    mask = cv.inRange(hsv, gris_claro, gris_oscuro)
+    mask = cv.inRange(hsv, verde_oscuro, verde_claro)
     
     # Bitwise-AND mask anf original image
     res = cv.bitwise_and(frame, frame, mask=mask)
