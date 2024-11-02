6366d4b4a13b1d3cb53285fd80109cc7632aa53e
RubenAG <rubenalfonsogamboa@gmail.com>
2024-11-02 03:58:25 -0600

    paletas de colores para las ventanas y re acomodo de directorios

---
+++ colores.py
@@ -0,0 +1,19 @@
+# Colección de colores
+# Crear una arreglo para guardar los colores
+
+## Propuesta 1 de Rubén
+white = "#FCFCFC"
+mindaro = "#D2F898"
+yellow = "#F6F930"
+yet = "#2F2F2F"
+black = "#000000"
+
+## Propuesta 2 de Ruben
+rusian_violet = "#38023B"
+tropical_indigo = "#A288E3"
+columbia_blue = "#BBD5ED"
+mint_green = "#CEFDFF"
+tea_green = "#CCFFCB"
+
+paleta1 = [white, mindaro, yellow, yet, black]
+paleta2 = [rusian_violet, tropical_indigo, columbia_blue, mint_green, tea_green]
--- inicio.py
+++ inicio.py
@@ -1,13 +1,32 @@
-#Ignorar por el momento chavoz
+'''
+Creadores:
+La flor
+El inge
+El samy
+El García²
+El Aleks
+Alfonso Gamboa Rubén
+'''
 
-#Creadores:
-# La flor
-# El inge
-# El samy
-# El García²
-# El Aleks
-# Alfonso Gamboa Rubén
-#Librerias: tkinter, custom tkinter
-#Propuesta de diseño:
+import tkinter as Tk
+import customtkinter as CTk
+import colores as col
 
-# Importaciones:
+#Colores escogidos
+paleta = col.paleta2
+c1 = paleta[0] # Color mas oscuro
+c2 = paleta[1] # 
+c3 = paleta[2] # 
+c4 = paleta[3] # 
+c5 = paleta[4] # Color más claro
+
+# Ventana principal
+v_principal = CTk() # En este caso va a ser un objeto de custom tkinter (para colores y diseños chidos)
+v_principal.geometry("500x600+350+20") # Geometría a libre albedrío
+v_principal.minsize(480, 50)
+v_principal.config(bg = c)
+
+
+
+
+v_principal.mainloop()
