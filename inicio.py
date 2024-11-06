6e3a6c7cdc526599b3d0cac58b32b0dfe550159f
RubenAG <rubenalfonsogamboa@gmail.com>
2024-11-06 01:46:26 -0600

    almenos es un avance

--- inicio.py
+++ inicio.py
@@ -9,24 +9,40 @@ Alfonso Gamboa Rubén
 '''
 
 import tkinter as Tk
-from customtkinter import CTk
+from customtkinter import CTk, CTkFrame, CTkEntry
 import colores as col
 
-#Colores escogidos
+# Colores escogidos
 paleta = col.paleta1
-c1 = paleta[0] # Para el fondo (o contraste fondo 2)
+c1 = paleta[0] # Para el fondo
 c2 = paleta[1] # Iconos 1
 c3 = paleta[2] # Iconos 2
-c4 = paleta[3] # fondo 2 (o contraste fondo 1)
+c4 = paleta[3] # para los marcos
 c5 = paleta[4] # Para las letras
+c_negro = "#000000" #Colores constantes
+c_blanco = "FFFFFF" #Colores constantes
 
 # Ventana principal
 v_principal = CTk() # En este caso va a ser un objeto de custom tkinter (para colores y diseños chidos)
-v_principal.geometry("500x600+350+20") # Geometría a libre albedrío
-v_principal.minsize(480, 50)
+v_principal.geometry("960x540") # Geometría a libre albedrío
+v_principal.minsize(320, 540) #tamaño mínimo para que no pasen cosas extrañas ;)
 v_principal.config(bg = c1)
 
+## configuración de filas y columnas para toda la ventana principal, permitiendonos centrar el contenido sin importar el tamaño de la ventana
+v_principal.columnconfigure(0, weight=1)
+v_principal.rowconfigure(0, weight=1)
 
+# marco
+marco1 = CTkFrame(v_principal, fg_color=c1, bg_color=c1) #le ponemos el mismo color que el de fondo de pantalla
+marco1.grid(column=0, row=0, sticky="nsew", padx=50, pady=50)
+## configuración de columnas y ventanas del marco
+marco1.columnconfigure([0, 1], weight=1)
+marco1.rowconfigure([0,1,2,3,4,5], weight=1) 
+
+
+#Entrada de nombre
+e_nombre = CTkEntry(marco1,font=("sans rerif", 14), bg_color=c1, fg_color= c4, text_color=c1, placeholder_text="Nombre", border_color=c4)
+e_nombre.grid(columnspan=2, row=1, padx=4, pady=4)
 
 
 v_principal.mainloop()
